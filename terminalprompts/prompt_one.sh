d987571c4ec96fadae086b267e30db25e8c66219
Arvindraj <arvindultimate7352@gmail.com>
2019-10-21 20:07:43 +0530

    Add Terminal Prompt scripts
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

---
+++ terminalprompts/prompt_one.sh
@@ -0,0 +1,23 @@
+function branch()
+{
+    if [[ -d .git ]] || [[ $(git rev-parse --git-dir 2> /dev/null) ]]; then
+        echo -en "\033[1;34m[$(git branch | head -1 | awk '{print $2}')]\033[0m";
+    fi
+}
+
+function exitcolor()
+{
+    if [[ "$?" == "0" ]]; then
+        echo -en "\033[1;33m";
+    else
+        echo -en "\033[1;31m";
+    fi
+}
+
+if [[ $(id -u) == 0 ]]; then
+        export PS1='$(exitcolor)[i.am.groot.]\033[0m\n\033[1m[\h]\033[1m[\w]$(branch) \[\033[1;35m\]#>\[\033[0m\] ';
+else
+        export PS1='$(exitcolor)[\u@\h]\[\033[0m\033[1m\][\w]$(branch)\n\[\033[1m\] ⤷\[\033[0m\] ';
+fi
+
+export PS2='\[\033[1;36m\]>\[\033[0m\] ';
---
+++ terminalprompts/prompt_two.sh
@@ -0,0 +1,24 @@
+function branch()
+{
+    if [[ -d .git ]] || [[ $(git rev-parse --git-dir 2> /dev/null) ]]; then
+        echo -en "\033[1;34mon [$(git branch | head -1 | awk '{print $2}')]\033[0m";
+    fi
+}
+
+function exitcolor()
+{
+    if [[ "$?" == "0" ]]; then
+        echo -en "\033[1;33m";
+    else
+        echo -en "\033[1;31m";
+    fi
+}
+
+if [[ $(id -u) == 0 ]]; then
+    export PS1='$(exitcolor)groot\[\033[0m\033[1m\] in [\h]\033[1m at \w $(branch) \[\033[1;35m\]#❯\[\033[0m\] ';
+else
+    export PS1='$(exitcolor)\u at \h\[\033[0m\033[1m\] in \w $(branch)\n\[\033[1;35m\]❯❯\[\033[0m\] ';
+fi
+
+export PS2='\[\033[1;36m\]❯❯\[\033[0m\] ';
+
